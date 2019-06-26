c5dba3ad843ab3a563faed6345186da085a4094b
Dmytro Ryabokon <dmytro.ryabokon@synapse.com>
2019-06-26 17:58:46 +0300

    maskRcnn

--- ex_07_MASK_RCNN.py
+++ ex_07_MASK_RCNN.py
@@ -7,7 +7,7 @@ folder_out = './data/output/'
 # ----------------------------------------------------------------------------------------------------------------------
 def example_Mask_RCNN_on_file():
 
-    filename_image = './data/ex16/LON/frame000777.jpg'
+    filename_image = './data/ex16g/LON/frame000300.jpg'
     D = detector_Mask_RCNN.detector_Mask_RCNN(filename_weights, folder_out)
     D.process_file(filename_image, 'data/output/res_MASK_RCNN.jpg',draw_spline=True)
 
@@ -15,7 +15,7 @@ def example_Mask_RCNN_on_file():
 # ----------------------------------------------------------------------------------------------------------------------
 def example_Mask_RCNN_on_folder():
 
-    folder_in = './data/ex16/LON/'
+    folder_in = './data/ex16g/LON/'
     D = detector_Mask_RCNN.detector_Mask_RCNN(filename_weights, folder_out)
     start_time = time.time()
     D.process_folder(folder_in, folder_out)
@@ -24,8 +24,8 @@ def example_Mask_RCNN_on_folder():
 # ----------------------------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
 
-    #example_Mask_RCNN_on_file()
-    example_Mask_RCNN_on_folder()
+    example_Mask_RCNN_on_file()
+    #example_Mask_RCNN_on_folder()
 
 
 
