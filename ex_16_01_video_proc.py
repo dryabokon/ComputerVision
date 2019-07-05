6beb6e77c0da4cff52ee7aea4e28d2691c1fabcf
Dmytro Ryabokon <dmytro.ryabokon@synapse.com>
2019-07-05 11:01:53 +0300

    ML

--- ex_16_01_video_proc.py
+++ ex_16_01_video_proc.py
@@ -1,10 +1,50 @@
 # ----------------------------------------------------------------------------------------------------------------------
 import tools_video
 import tools_animation
+import tools_image
+# ----------------------------------------------------------------------------------------------------------------------
+def cap_01():
+    prefix = '01_'
+    filename_in = 'D:/Projects/Nice/streets/London01.mp4'
+    folder_out = 'D:/Projects/Nice/streets/'+prefix+'/'
+    tools_video.extract_frames(filename_in, folder_out, prefix=prefix,start_time_sec=60 * 35 + 9, end_time_sec=60 * 35 + 20)
+    return
+# ----------------------------------------------------------------------------------------------------------------------
+def cap_02():
+    prefix = '02_'
+    filename_in = 'D:/Projects/Nice/streets/London01.mp4'
+    folder_out = 'D:/Projects/Nice/streets/'+prefix+'/'
+    tools_video.extract_frames(filename_in, folder_out,prefix=prefix, start_time_sec=60 * 43 + 25, end_time_sec=60 * 43 + 40)
+    return
+# ----------------------------------------------------------------------------------------------------------------------
+def cap_03():
+    prefix = '03_'
+    filename_in = 'D:/Projects/Nice/streets/London02.mp4'
+    folder_out = 'D:/Projects/Nice/streets/'+prefix+'/'
+    tools_video.extract_frames(filename_in, folder_out,prefix=prefix, start_time_sec=60 * 16 + 10, end_time_sec=60 * 16 + 26)
+    return
+# ----------------------------------------------------------------------------------------------------------------------
+def cap_04():
+    prefix = '04_'
+    filename_in = 'D:/Projects/Nice/streets/London02.mp4'
+    folder_out = 'D:/Projects/Nice/streets/'+prefix+'/'
+    tools_video.extract_frames(filename_in, folder_out,prefix=prefix, start_time_sec=60*60*1 + 60 * 12 + 20, end_time_sec=60*60*1 + 60 * 12 + 30)
+    return
+# ----------------------------------------------------------------------------------------------------------------------
+def cap_05():
+    prefix = '05_'
+    filename_in = 'D:/Projects/Nice/streets/London02.mp4'
+    folder_out = 'D:/Projects/Nice/streets/'+prefix+'/'
+    tools_video.extract_frames(filename_in, folder_out,prefix=prefix, start_time_sec= 60 * 24 + 10, end_time_sec= 60 * 24 + 25)
+    return
 # ----------------------------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
-    path_input = './data/output/'
-    #path_input = 'D:/Projects/Telefonica/flow2/'
-    filename_out = path_input + 'anim.gif'
+    cap_01()
+    cap_02()
+    cap_03()
+    cap_04()
+    cap_05()
 
-    tools_animation.folder_to_animated_gif_imageio(path_input, filename_out, mask='*.jpg', framerate=60,resize_H=360, resize_W=640)
+
+    #tools_animation.crop_images_in_folder(path_input,path_output,top, left, bottom, right)
+    #tools_animation.folder_to_animated_gif_imageio(path_input, filename_out, mask='*.jpg', framerate=40,resize_H=1200//4, resize_W=2154//4)
