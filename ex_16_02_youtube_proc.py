1b28da6dc8aeb1e4323a38412b0983656e042b7e
Dmytro Ryabokon <dmytro.ryabokon@synapse.com>
2019-08-30 13:26:39 +0300

    dummy

--- ex_16_02_youtube_proc.py
+++ ex_16_02_youtube_proc.py
@@ -3,4 +3,5 @@ import tools_video
 # ----------------------------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
 
-    tools_video.grab_youtube_video('https://www.youtube.com/watch?v=ibIKS84ETpM','./data/output/res.mp4')
+    #tools_video.grab_youtube_video('https://www.youtube.com/watch?v=ibIKS84ETpM','./data/output/res.mp4')
+    tools_video
