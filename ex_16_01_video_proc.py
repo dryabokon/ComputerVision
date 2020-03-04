d1d621a45bbdc216061fc5b05c4ed6527482755c
Dima <d.ryabokon@gmail.com>
2020-03-04 19:23:38 +0200

    lines

--- ex_16_01_video_proc.py
+++ ex_16_01_video_proc.py
@@ -3,41 +3,6 @@ import tools_video
 import tools_animation
 import tools_image
 # ----------------------------------------------------------------------------------------------------------------------
-def cap_01():
-    prefix = '01_'
-    filename_in = 'D:/Projects/Nice/streets/London01.mp4'
-    folder_out = 'D:/Projects/Nice/streets/'+prefix+'/'
-    tools_video.extract_frames(filename_in, folder_out, prefix=prefix,start_time_sec=60 * 35 + 9, end_time_sec=60 * 35 + 20)
-    return
-# ----------------------------------------------------------------------------------------------------------------------
-def cap_02():
-    prefix = '02_'
-    filename_in = 'D:/Projects/Nice/streets/London01.mp4'
-    folder_out = 'D:/Projects/Nice/streets/'+prefix+'/'
-    tools_video.extract_frames(filename_in, folder_out,prefix=prefix, start_time_sec=60 * 43 + 25, end_time_sec=60 * 43 + 40)
-    return
-# ----------------------------------------------------------------------------------------------------------------------
-def cap_03():
-    prefix = '03_'
-    filename_in = 'D:/Projects/Nice/streets/London02.mp4'
-    folder_out = 'D:/Projects/Nice/streets/'+prefix+'/'
-    tools_video.extract_frames(filename_in, folder_out,prefix=prefix, start_time_sec=60 * 16 + 10, end_time_sec=60 * 16 + 26)
-    return
-# ----------------------------------------------------------------------------------------------------------------------
-def cap_04():
-    prefix = '04_'
-    filename_in = 'D:/Projects/Nice/streets/London02.mp4'
-    folder_out = 'D:/Projects/Nice/streets/'+prefix+'/'
-    tools_video.extract_frames(filename_in, folder_out,prefix=prefix, start_time_sec=60*60*1 + 60 * 12 + 20, end_time_sec=60*60*1 + 60 * 12 + 30)
-    return
-# ----------------------------------------------------------------------------------------------------------------------
-def cap_05():
-    prefix = '05_'
-    filename_in = 'D:/Projects/Nice/streets/London02.mp4'
-    folder_out = 'D:/Projects/Nice/streets/'+prefix+'/'
-    tools_video.extract_frames(filename_in, folder_out,prefix=prefix, start_time_sec= 60 * 24 + 10, end_time_sec= 60 * 24 + 25)
-    return
-# ----------------------------------------------------------------------------------------------------------------------
 def cap_06():
     URL = 'https://www.youtube.com/watch?v=ulpIL6KhD50'
     out_path = 'D:/'
@@ -46,36 +11,20 @@ def cap_06():
     return
 # ----------------------------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
-    #cap_01()
-    #cap_02()
-    #cap_03()
-    #cap_04()
-    #cap_05()
-    #cap_06()
-
     #filename_in = 'D:/Projects/VFS/Cashiers 1_IP169_port/02.07.2019 14_59_59 (UTC+03_00).mkv'
     #folder_out = 'D:/Projects/VFS/output/'
-
 
     #tools_video.extract_frames('D:/res.mp4','D:/3/',prefix='',start_time_sec=70)
     #tools_animation.crop_images_in_folder(path_input,path_output,115, 123, 832, 1400)
     #tools_animation.folder_to_animated_gif_imageio('D:/1/', 'D:/ani.gif', mask='*.jpg', framerate=25,resize_W=210,resize_H=120)
 
-    #tools_video.extract_frames('D:/Z.avi','D:/Z/',prefix='')
-    #tools_animation.crop_images_in_folder('D:/2/','D:/2c/',0, 800, 1080, 1920)
-    #tools_animation.merge_images_in_folders('D:/2/','D:/3/','D:/res/')
-
     #tools_animation.folder_to_animated_gif_imageio('D:/JB/', 'D:/JB.gif', mask='*.jpg', framerate=25,resize_W=1120//4,resize_H=540//4)
     #tools_animation.folder_to_video('D:/Projects/Telefonica/flow3_croped/all/','D:/TF_ani4.mp4',mask='*.jpg',resize_W=2154//4,resize_H=1200//4)
 
 
-<<<<<<< HEAD
-    #tools_video.extract_frames('D:/res_merged_v01.mp4','D:/JB/',prefix='')
 
-    tools_animation.folder_to_video('D:/Projects/Telefonica/flow3_croped/all/', 'D:/TF_ani4.mp4', mask='*.jpg',resize_W=2154 // 4, resize_H=1200 // 4)
-=======
-    tools_video.grab_youtube_video('https://www.youtube.com/watch?v=CAUUi8zEZBk','D:/','bbc.avi')
+    #tools_animation.folder_to_video('D:/Projects/Telefonica/flow3_croped/all/', 'D:/TF_ani4.mp4', mask='*.jpg',resize_W=2154 // 4, resize_H=1200 // 4)
+    #tools_video.grab_youtube_video('https://www.youtube.com/watch?v=CAUUi8zEZBk','D:/','bbc.avi')
 
 
-
->>>>>>> 871a9343f4348494f6c8c73d657ed1b9e20c77b6
+    tools_video.extract_frames('D:/output_sign_gates.avi','D:/F/',prefix='')
--- ex_23_line_detection.py
+++ ex_23_line_detection.py
@@ -1,110 +1,18 @@
 import cv2
-import numpy
-from skimage.morphology import skeletonize
-import matplotlib.pyplot as plt
-from skimage import data
-import sknw
-from numba.errors import NumbaWarning
-import warnings
-warnings.simplefilter('ignore', category=NumbaWarning)
+import tools_soccer_field
 #----------------------------------------------------------------------------------------------------------------------
-import tools_image
-import tools_filter
-#----------------------------------------------------------------------------------------------------------------------
-def line_length(x1, y1, x2, y2):return numpy.sqrt((x1-x2)**2 + (y1-y2)**2)
-# ----------------------------------------------------------------------------------------------------------------------
-blur_kernel = 2
-min_line_len = 100
-# ----------------------------------------------------------------------------------------------------------------------
 filename_in = './data/ex_lines/Image1.jpg'
-#filename_in = './data/ex_lines/H.png'
-folder_out = './data/output/'
+#filename_in = './data/ex_lines/frame001710.jpg'
+SFP = tools_soccer_field.Soccer_Field_Processor()
 # ----------------------------------------------------------------------------------------------------------------------
-def detect_01(filename_in,folder_out,blur_kernel=5):
-    img = cv2.imread(filename_in)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    blur = tools_filter.sliding_2d(gray,blur_kernel,blur_kernel,stat='avg',mode='reflect').astype(numpy.uint8)
-    threshholded = 255-cv2.adaptiveThreshold(255-blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY, 11, 2)
-
-    #edges = threshholded
-    #edges = cv2.Canny(threshholded, 10, 50, apertureSize=3)
-    edges = (255*skeletonize(threshholded/255)).astype(numpy.uint8)
-
-
-    lines = cv2.HoughLinesP(edges, 1, numpy.pi / 180, 40, min_line_len)
-
-    result = img.copy()
-    result = tools_image.desaturate(result)
-    res_bin = numpy.zeros(img.shape,dtype=numpy.uint8)
-    for line in lines:
-        for x1, y1, x2, y2 in line:
-            if (line_length(x1, y1, x2, y2) > 0 * min_line_len):
-                result = cv2.line(result, (x1, y1), (x2, y2), (0, 32, 255), 2)
-                res_bin = cv2.line(res_bin, (x1, y1), (x2, y2), (255, 255, 255), 2)
-
-    cv2.imwrite(folder_out + '1-blur.png', blur)
-    cv2.imwrite(folder_out + '2-threshholded.png', threshholded)
-    cv2.imwrite(folder_out + '3-edges.png', edges)
-    cv2.imwrite(folder_out + '4-res.png', result)
-    cv2.imwrite(folder_out + '5-res_bin.png', res_bin)
-    return
-# ----------------------------------------------------------------------------------------------------------------------
-def detect_02(filename_in,folder_out):
-    img = cv2.imread(filename_in)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    edges = cv2.Canny(gray, 50, 150, apertureSize=3)
-    result = img.copy()
-    result = tools_image.desaturate(result)
-
-    lines = cv2.HoughLines(edges, 1, numpy.pi / 180, min_line_len)
-
-    for line in lines:
-        for rho, theta in line:
-            a = numpy.cos(theta)
-            b = numpy.sin(theta)
-            x0 = a * rho
-            y0 = b * rho
-            x1 = int(x0 - 100 * b)
-            y1 = int(y0 + 100 * a)
-            x2 = int(x0 + 100 * b)
-            y2 = int(y0 - 100 * a)
-
-
-            cv2.line(result, (x1, y1), (x2, y2), (0, 12, 255), 2)
-
-    cv2.imwrite(folder_out + 'res02.png', result)
-    return
-# ----------------------------------------------------------------------------------------------------------------------
-def detect_03(filename_in,folder_out):
-
-    img = cv2.imread(filename_in)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    blur = tools_filter.sliding_2d(gray, blur_kernel, blur_kernel, stat='avg', mode='reflect').astype(numpy.uint8)
-    threshholded = 255 - cv2.adaptiveThreshold(255 - blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11,2)
-    ske = skeletonize(threshholded>0).astype(numpy.uint16)
-    result  = img.copy()
-    result = tools_image.desaturate(result)
-
-    graph = sknw.build_sknw(ske)
-
-    for (s, e) in graph.edges():
-        ps = graph[s][e]['pts']
-        xx = ps[:, 1]
-        yy = ps[:, 0]
-        if len(xx)>20 and line_length(xx[0],yy[0],xx[-1],yy[-1]) > 50:
-            for i in range(len(xx)-1):
-                result = cv2.line(result, (xx[i],yy[i]), (xx[i+1],yy[i+1]), (0, 32, 255),thickness=4)
-                #result = cv2.circle(result, (int(ps[:, 1][i]), int(ps[:, 0][i])), 3, (255, 128, 0), -1)
-
-
-    cv2.imwrite(folder_out + '4-res.png', result)
-    plt.show()
+def example_01(filename_in):
+    image = cv2.imread(filename_in)
+    skeleton = SFP.skelenonize_slow(image,do_debug=True)
+    SFP.get_hough_lines(skeleton,do_debug=True)
     return
 # ----------------------------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
 
-    #detect_01(filename_in, folder_out,blur_kernel=4)
-    #detect_01(folder_out + 'input.png', folder_out,blur_kernel=4)
-    #detect_02(folder_out + '5-res_bin.png', folder_out)
-    detect_03(filename_in,folder_out)
+    example_01(filename_in)
+
 
