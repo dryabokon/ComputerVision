9c53894435dcbcaf575bdea153d74853c978ec25
Dima <d.ryabokon@gmail.com>
2020-03-05 07:55:19 +0200

    lines

--- ex_23_line_detection.py
+++ ex_23_line_detection.py
@@ -1,14 +1,16 @@
+import numpy
 import cv2
 import tools_soccer_field
+from PIL import Image, ImageDraw, ImageOps
 #----------------------------------------------------------------------------------------------------------------------
-filename_in = './data/ex_lines/Image1.jpg'
-#filename_in = './data/ex_lines/frame001710.jpg'
+filename_in = './data/ex_lines/frame000269.jpg'
 SFP = tools_soccer_field.Soccer_Field_Processor()
 # ----------------------------------------------------------------------------------------------------------------------
 def example_01(filename_in):
     image = cv2.imread(filename_in)
     skeleton = SFP.skelenonize_slow(image,do_debug=True)
     SFP.get_hough_lines(skeleton,do_debug=True)
+
     return
 # ----------------------------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
