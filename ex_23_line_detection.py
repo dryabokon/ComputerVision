c77820f01b2296bbfe2caaeec7d7616f45ea6ed5
Dmytro Ryabokon <dmytro.ryabokon@synapse.com>
2020-03-06 18:35:17 +0200

    lines

--- ex_23_line_detection.py
+++ ex_23_line_detection.py
@@ -4,14 +4,16 @@ import numpy
 import cv2
 import tools_soccer_field
 import tools_image
+import tools_IO
 # ----------------------------------------------------------------------------------------------------------------------
+folder_in = './data/ex_lines/'
 folder_out = './data/output/'
 filename_in = './data/ex_lines/frame000626.jpg'
-folder_in = './data/ex_lines/'
-filename_in = './data/ex_lines/Image1.jpg'
+#filename_in = './data/ex_lines/Image1.jpg'
 SFP = tools_soccer_field.Soccer_Field_Processor()
 # ----------------------------------------------------------------------------------------------------------------------
 def example_01(filename_in,do_debug=True):
+    tools_IO.remove_files(folder_out,create=True)
 
     image = cv2.imread(filename_in)
     result = SFP.process_left_view(image,do_debug)
@@ -26,5 +28,3 @@ if __name__ == '__main__':
 
     example_01(filename_in)
     #example_02(folder_in,folder_out)
-
-
